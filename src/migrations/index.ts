8a2312128b51c603abb609d9fba2fc1511118601
Deodatus Biche <bichesq@gmail.com>
2026-03-31 19:11:17 +0530

    chore: register the new courseMedia migration in the migrations index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260304_023444_initial_schema from './20260304_023444_initial_schema';
 import * as migration_20260304_065827_add_img_name_to_slides from './20260304_065827_add_img_name_to_slides';
+import * as migration_20260331_171500_add_course_media from './20260331_171500_add_course_media';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260304_065827_add_img_name_to_slides.up,
     down: migration_20260304_065827_add_img_name_to_slides.down,
     name: '20260304_065827_add_img_name_to_slides'
+  },
+  {
+    up: migration_20260331_171500_add_course_media.up,
+    down: migration_20260331_171500_add_course_media.down,
+    name: '20260331_171500_add_course_media'
   },
 ];
